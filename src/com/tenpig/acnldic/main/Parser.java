bd32e6bb6de5aaf41079188fe912377084454485
Kyounghun Kim <khkim@sejong.knu.ac.kr>
2016-02-03 16:57:53 +0900

    gg

--- src/com/tenpig/acnldic/main/Parser.java
+++ src/com/tenpig/acnldic/main/Parser.java
@@ -8,21 +8,36 @@ import java.util.regex.Pattern;
 public class Parser {
 	static final String resPath = "resources/";
 
+	public String getBetween(String b, String e, String s) {
+		return s.substring(s.indexOf(b) + b.length(), s.indexOf(e, s.indexOf(b) + b.length()));
+	}
+
 	public void run() throws Exception {
 		BufferedReader br = new BufferedReader(new FileReader(resPath + "dic.txt"));
 		String s = null;
 		String[] buf = null;
 		Pattern p = Pattern.compile("<.*?>");
+		String ptnNameBegin = "<span class=\"catch-name\">";
+		String ptnNameEnd = "</span>";
+
 		int c = 0;
 		while ((s = br.readLine()) != null) {
 			if (s.contains("<!-- -->")) {
 				System.out.println("----------------------");
-				c++;
+				
 
 			}
-			System.out.println(s.replaceAll("<.*?>", ""));
-			Matcher m = p.matcher(s);
+			if (s.contains(ptnNameBegin)) {
+			//	System.out.println(getBetween(ptnNameBegin, ptnNameEnd, s));
+				c++;
+			}
 			
+			s=s.replaceAll("<.*?>", "").trim();
+			if(s.length()==0) continue;
+			
+			 System.out.println(s);
+			Matcher m = p.matcher(s);
+
 		}
 		System.out.println(c);
 		br.close();
